dab3331bd155bad7ec9d5c70dc76f5e08c03dbc0
disha rajput <dishacsevira@gmail.com>
2025-02-07 17:33:53 +0530

    create js calculater

---
+++ 01_calculater/script.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+  let results = a + b;
+  return results;
+};
+console.log(add(10, 2));
+
+const sub = (n, m) => {
+  let results = n - m;
+  return results;
+};
+console.log(sub(10, 2));
+
+const multiple = (l, o) => {
+  let results = l * o;
+  return results;
+};
+console.log(multiple(10, 2));
+
+const division = (p, q) => {
+  let results = p / q;
+  return results;
+};
+console.log(division(10, 2));
